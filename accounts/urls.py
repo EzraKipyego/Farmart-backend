742fdcccc8fed3fd9a1f0274df72bbe79cfe152a
EZRA KIPYEGO <ezra.kipyego@student.moringaschool.com>
2026-08-26 09:44:27 +0300

    added auth url routes

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import RegisterView, LoginView, ProfileView
+
+urlpatterns = [
+    path("auth/register", RegisterView.as_view()),
+    path("auth/login", LoginView.as_view()),
+    path("profile", ProfileView.as_view()),
+]
