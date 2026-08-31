9837117b5a165a0a088c2730d3b2fad6ff835df1
EZRA KIPYEGO <ezra.kipyego@student.moringaschool.com>
2026-08-31 21:53:26 +0300

    add password reset authentication routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from .views import LoginView, ProfileView, RegisterView
+from .views import (
+    LoginView,
+    PasswordResetConfirmView,
+    PasswordResetRequestView,
+    ProfileView,
+    RegisterView,
+)
 
 urlpatterns = [
     path("auth/register", RegisterView.as_view()),
     path("auth/login", LoginView.as_view()),
+    path("auth/password-reset", PasswordResetRequestView.as_view()),
+    path("auth/password-reset/confirm", PasswordResetConfirmView.as_view()),
     path("profile", ProfileView.as_view()),
 ]
